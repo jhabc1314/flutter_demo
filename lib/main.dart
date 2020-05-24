cc4918b2dc5f002751e64f4ffa63ee8efef88a39
江恒 <jiangheng@chelun.com>
2020-05-24 19:32:53 +0800

    master

--- lib/main.dart
+++ lib/main.dart
@@ -3,90 +3,54 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Widget",
-    home: MyApp(),
+    home: Counter(),
   ));
 }
 
-class Mybutton extends StatelessWidget {
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
+  final int count;
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => print("mybutton was tapped"),
-      child: Container(
-        height: 36,
-        padding: const EdgeInsets.all(8.0),
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5.0),
-          color: Colors.lightGreen[500],
-        ),
-        child: Center(),
-      ),
-    );
-
+  Widget build(BuildContext context){
+    return Text('count:$count');
   }
 }
-class MyApp extends StatelessWidget {
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+  final VoidCallback onPressed;
+
+
   @override
   Widget build(BuildContext context) {
-    var appBar = AppBar(
-      leading: IconButton(
-        icon: Icon(Icons.menu),
-        tooltip: 'Navigation Menu',
-        onPressed: null,
-      ),
-      title: Text("Example 标题"),
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          tooltip: '搜索',
-          onPressed: null,
-        )
-      ],
-    );
-    var body = Center(
-      child: Text("你好 世界"),
-    );
-    var floatActionButton = FloatingActionButton(
-      tooltip: 'Add',
-      child: Icon(Icons.add),
-      onPressed: null,
-    );
-
-    //赋值
-    return Scaffold(
-      appBar: appBar,
-      body: body,
-      floatingActionButton: floatActionButton,
+    // TODO: implement build
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text("Increment"),
     );
   }
 }
 
 class Counter extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return
-  }
+  _CounterState createState() => _CounterState();
 }
 
 class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
+  int _count = 0;
+  void _increment () {
     setState(() {
-      _counter++;
+      _count++;
     });
   }
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
+  Widget build (BuildContext context) {
     return Row(
       children: <Widget>[
-        RaisedButton(onPressed: _increment,child: Text('increment'),),
-        Text('Count:$_counter')
+        CounterIncrementor(onPressed: _increment,),
+        CounterDisplay(count: _count,)
       ],
-    )
+    );
   }
 }
